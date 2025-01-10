c055eaa185a7dd924a49ac5256c1f729cb11d2ea
chloe900 <chloetdube@icloud.com>
2025-01-10 20:49:25 +0200

    node.js

--- requests.js
+++ requests.js
@@ -1,15 +1,15 @@
-import express from 'express';
-import { urlencoded } from 'body-parser';
-const app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
 
-app.use(urlencoded({ extended: true })); 
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/formData', (req, res) => {
-  res.send(`Data from form :${req.body.name} ${req.body.email}${req.body.message}.`);
+  res.send(`Data from form: ${req.body.name} ${req.body.email} ${req.body.message}.`);
 });
 
-const port = 5500;
+const port = 8080;
 
 app.listen(port, () => {
-  console.log(`Your server is running on port${port}`);
+  console.log(`Your server is running on port ${port}`);
 });
